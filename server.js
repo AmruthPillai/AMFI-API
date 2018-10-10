79e1410b366fa8ee79a0f01a95d91cfda1df6597
Amruth Pillai <im.amruth@gmail.com>
2018-10-11 01:44:01 +0530

    initial server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
