632e297c3ce140913f50eecb698734c79a3b82e9
Amruth Pillai <im.amruth@gmail.com>
2018-10-11 10:43:45 +0530

    created routes

--- api.js
+++ api.js
@@ -3,62 +3,75 @@ var router = express.Router()
 
 const request = require('request')
 
-const fundData = [];
-const apiEndpoint = 'http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?mf=3&tp=1&frmdt=01-Oct-2018&todt=10-Oct-2018';
+function parseData(body) {
+  fundData = []
+  bodyClean = body.replace(/\r?\n/g, "\n")
+  bodyArr = bodyClean.split("\n")
+  funds = bodyArr.map((str) => {
+    return str.split(";")
+  })
+
+  headers = funds[0]
+
+  for (let i = 1; i < funds.length; i++) {
+    if (funds[i].length === 6 && !funds[i][3].includes("Direct")) {
+      let obj = {}
+      for (let j = 0; j < 6; j++)
+        obj[headers[j]] = funds[i][j]
+      fundData.push(obj)
+    }
+  }
+
+  return fundData
+}
 
 router.get('/', function (req, res) {
-  request(apiEndpoint, function (error, response, body) {
-    if (error) console.error(error)
+  const url = 'https://www.amfiindia.com/spages/NAVAll.txt'
 
-    bodyClean = body.replace(/\r?\n/g, "\n");
-    bodyArr = bodyClean.split("\n");
-    funds = bodyArr.map((str) => {
-      return str.split(";");
-    });
+  request(url, function (error, response, body) {
+    const fundData = parseData(body)
 
-    headers = funds[0];
-
-    for (let i = 1; i < funds.length; i++) {
-      if (funds[i].length === 6 && !funds[i][1].includes("Direct") && funds[i]) {
-        let obj = {};
-        for (let j = 0; j < 6; j++)
-          obj[headers[j]] = funds[i][j];
-        fundData.push(obj);
-      }
-    }
-
-    res.json(fundData);
+    res.json(fundData)
   })
 })
 
 router.get('/:schemeCode', function (req, res) {
-  console.log(req.params.schemeCode);
+  const url = 'https://www.amfiindia.com/spages/NAVAll.txt'
 
-  request(apiEndpoint, function (error, response, body) {
-    if (error) console.error(error)
+  request(url, function (error, response, body) {
+    let fundData = parseData(body)
 
-    bodyClean = body.replace(/\r?\n/g, "\n");
-    bodyArr = bodyClean.split("\n");
-    funds = bodyArr.map((str) => {
-      return str.split(";");
-    });
-
-    headers = funds[0];
-
-    for (let i = 1; i < funds.length; i++) {
-      if (funds[i].length === 6 && !funds[i][1].includes("Direct") && funds[i]) {
-        let obj = {};
-        for (let j = 0; j < 6; j++)
-          obj[headers[j]] = funds[i][j];
-        fundData.push(obj);
-      }
-    }
-
-    let resData = fundData.filter((fund) => {
-      return fund['Scheme Code'] === req.params.schemeCode;
+    fundData = fundData.filter((fund) => {
+      if (fund['Scheme Code'] == req.params.schemeCode)
+        return fund
     })
 
-    res.json(resData);
+    res.json(fundData[0])
+  })
+})
+
+router.get('/:startDate/:endDate', function (req, res) {
+  const url = 'http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?tp=1&frmdt=' + req.params.startDate + '&todt=' + req.params.endDate
+
+  request(url, function (error, response, body) {
+    const fundData = parseData(body)
+
+    res.json(fundData)
+  })
+})
+
+router.get('/:startDate/:endDate/:schemeCode', function (req, res) {
+  const url = 'http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?tp=1&frmdt=' + req.params.startDate + '&todt=' + req.params.endDate
+
+  request(url, function (error, response, body) {
+    let fundData = parseData(body)
+
+    fundData = fundData.filter((fund) => {
+      if (fund['Scheme Code'] == req.params.schemeCode)
+        return fund
+    })
+
+    res.json(fundData)
   })
 })
 
