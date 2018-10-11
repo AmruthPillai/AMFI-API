cd95dae3c81342d0048ae2ee2dac45791953d4da
Amruth Pillai <im.amruth@gmail.com>
2018-10-11 11:07:22 +0530

    heroku port access

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var api = require('./api')
 
 app.use('/', api)
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
   var host = server.address().address
   var port = server.address().port
 
